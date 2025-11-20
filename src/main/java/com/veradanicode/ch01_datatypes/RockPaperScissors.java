1d99bf3e02e10dee7cebda73f956e0d7e2780fd9
Vera D <veradanicode@gmail.com>
2025-11-20 13:12:55 +0100

    update rockpaperscissors

--- src/main/java/com/veradanicode/ch01_datatypes/RockPaperScissors.java
+++ src/main/java/com/veradanicode/ch01_datatypes/RockPaperScissors.java
@@ -1,4 +1,20 @@
 package com.veradanicode.ch01_datatypes;
+import java.util.Scanner;
+import java.util.Random;
+
 
 public class RockPaperScissors {
+    public static void main(String[] args) {
+        String[] Options ={"ROCK","PAPER","SCISSORS"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Pick your number:ROCK(0)  PAPER(1) SCISSORS(2)");
+        int You =scanner.nextInt();
+
+        Random random = new Random();
+        int computerChoice=random.nextInt(3);
+
+
+
+
+    }
 }
