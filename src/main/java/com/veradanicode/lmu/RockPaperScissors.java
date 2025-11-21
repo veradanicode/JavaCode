3c4cedb2336eae5251bcd443eb664ff8843a29bc
Vera D <veradanicode@gmail.com>
2025-11-21 12:21:54 +0100

    update rock,paper,scissors game

--- src/main/java/com/veradanicode/lmu/RockPaperScissors.java
+++ src/main/java/com/veradanicode/lmu/RockPaperScissors.java
@@ -3,12 +3,54 @@ import java.util.Scanner;
 
 public class RockPaperScissors {
     public static void main(String[] args) {
-//        Scanner scanner =new Scanner(System.in);
-//        String playerInput=scanner.nextLine();
+     Scanner scanner= new Scanner(System.in);
+        System.out.println("ROCK,PAPER,SCISORS?? SHOOT!!");
+     String playerInput=scanner.nextLine();
+     String computerInput="";
 
-        String computerInput;
-        double num=(int)(3*Math.random());
-        System.out.println(num);
+     switch ((int)(3*Math.random())){
+         case 0:
+             computerInput="Rock";
+             break;
+         case 1:
+             computerInput="Paper";
+             break;
+         case 2:
+             computerInput="Scissors";
+             break;
+         default:
+             System.out.println("Invalid Input!");
+     }
+
+     if (playerInput.equalsIgnoreCase("Rock")){
+         if (computerInput.equalsIgnoreCase("Rock")){
+             System.out.println("YOU: "+playerInput+"\nComputer: "+computerInput+"\n It's a tie");
+         } else if (computerInput.equalsIgnoreCase("Paper")) {
+             System.out.println("YOU: "+playerInput+"\nComputer: "+computerInput+"\n  Computer wins");
+         }else {
+             System.out.println("YOU: "+playerInput+"\nComputer: "+computerInput+"\n you win");
+         }
+     }
+
+        if (playerInput.equalsIgnoreCase("Paper")){
+            if (computerInput.equalsIgnoreCase("Rock")){
+                System.out.println("YOU: "+playerInput+"\nComputer: "+computerInput+"\n  You win");
+            } else if (computerInput.equalsIgnoreCase("Paper")) {
+                System.out.println("YOU: "+playerInput+"\nComputer: "+computerInput+"\n  It's a Tie");
+            }else {
+                System.out.println("YOU: "+playerInput+"\nComputer: "+computerInput+"\n  Computer wins");
+            }
+        }
+
+        if (playerInput.equalsIgnoreCase("Scissors")){
+            if (computerInput.equalsIgnoreCase("Rock")){
+                System.out.println("YOU: "+playerInput+"\nComputer: "+computerInput+"\n Computer wins");
+            } else if (computerInput.equalsIgnoreCase("Paper")) {
+                System.out.println("YOU: "+playerInput+"\nComputer: "+computerInput+"\n  You win");
+            }else {
+                System.out.println("YOU: "+playerInput+"\nComputer: "+computerInput+"\n  It'a a tie");
+            }
+        }
 
     }
 }
